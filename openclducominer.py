ca60330a115abfc5007ffed3578d92c67616e181
YeahNotSewerSide <47860375+DoctorEenot@users.noreply.github.com>
2021-05-06 13:58:59 +0300

    Add files via upload

--- openclducominer.py
+++ openclducominer.py
@@ -106,7 +106,7 @@ def main(argv):
         
         expected_hash = bytearray.fromhex(job[1])
 
-        job_amount = 20000
+        job_amount = 50000
         
 
         if difficulty*100 < job_amount:
@@ -146,17 +146,16 @@ def main(argv):
             #        sendresult(result+i,timeDifference,difficulty)
             #        stop_mining = True
             #        break
-            for i in range(len(ducos)):
-                if ducos[i] == 1:
-                    hashingStopTime = time.time()
-                    timeDifference = hashingStopTime - hashingStartTime
-                    sendresult(result+i,timeDifference,difficulty)
-                    stop_mining = True
-                    break
+            res = numpy.where(ducos==1)[0]
+            if len(res!=0):
+                hashingStopTime = time.time()
+                timeDifference = hashingStopTime - hashingStartTime
+                sendresult(result+res[0],timeDifference,difficulty)
+                stop_mining = True
             #print('Checks Time:',time.time()-start_checks)
             if stop_mining:
                 break
-        print('LOOP END')
+        #print('LOOP END')
             
             
 
