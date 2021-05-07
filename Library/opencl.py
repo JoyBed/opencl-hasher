4cecdb4b5cf9301a96fcb88a8047f085c5241e26
YeahNotSewerSide <47860375+DoctorEenot@users.noreply.github.com>
2021-05-07 20:03:28 +0300

    Changed output data

--- Library/opencl.py
+++ Library/opencl.py
@@ -18,6 +18,7 @@ from binascii import unhexlify
 from collections import deque
 import time
 import os, sys, inspect
+import math
 
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
@@ -52,12 +53,12 @@ class opencl_interface:
         # Show devices for the platform, and adjust workgroup size
         # Create the context for GPU/CPU
         # Adjust workgroup size so that we don't run out of RAM:
-        self.sworkgroupsize = self.determine_workgroupsize(N_value)
+        #self.sworkgroupsize = self.determine_workgroupsize(N_value)
         self.inv_memory_density = inv_memory_density
         self.ctx = cl.Context(devices)
         self.queue = cl.CommandQueue(self.ctx, devices[openclDevice])
         self.debug = debug
-
+        self.available_memmory = 0
         for device in devices:
             printif(debug, '--------------------------------------------------------------------------')
             printif(debug, ' Device - Name: ' + device.name)
@@ -68,22 +69,22 @@ class opencl_interface:
             printif(debug, ' Device - Local memory size:  {}'.format(device.local_mem_size))
             printif(debug, ' Device - Max clock frequency: {} MHz'.format(device.max_clock_frequency))
 
-            assert device.endian_little == 1, "DEVICE is not little endian : pretty sure we rely on this!"
-            if self.workgroupsize == 0:
-                self.workgroupsize = maxWorkgroupSize
-                self.workgroupsize = min(self.workgroupsize, device.max_work_group_size)
-            else:
-                self.workgroupsize = min(self.workgroupsize, device.max_work_group_size)
+            #assert device.endian_little == 1, "DEVICE is not little endian : pretty sure we rely on this!"
+            #if self.workgroupsize == 0:
+            #    self.workgroupsize = maxWorkgroupSize
+            #    self.workgroupsize = min(self.workgroupsize, device.max_work_group_size)
+            #else:
+            #    self.workgroupsize = min(self.workgroupsize, device.max_work_group_size)
 
-            if self.computeunits == 0:
-                self.computeunits = device.max_compute_units
-            else:
-                self.computeunits = min(self.computeunits, device.max_compute_units)
-
+            #if self.computeunits == 0:
+            #    self.computeunits = device.max_compute_units
+            #else:
+            #    self.computeunits = min(self.computeunits, device.max_compute_units)
+            self.available_memmory += device.global_mem_size
             # if device.max_work_group_size<self.workgroupsize:
             #    self.workgroupsize=device.max_work_group_size
 
-        self.workgroupsize *= 1000
+        #self.workgroupsize *= 1000
         printif(debug, "\nUsing work group size of %d\n" % self.workgroupsize)
 
         # Set the debug flags
@@ -159,12 +160,9 @@ class opencl_interface:
                 break
 
             assert len(dk) == BLOCK_LEN_BYTES
-            #   , "Derived key input is length {}, when we expected {}".format(len(dk), BLOCK_LEN_BYTES)
 
             inpArray.extend(dk)
 
-        # pyopencl doesn't like empty buffers, so just cheer it up
-        #   (making the buffer larger isn't an issue)
         if len(inpArray) == 0:
             inpArray = b"\x00"
 
@@ -196,6 +194,32 @@ class opencl_interface:
         outBufSize_bytes = bufStructs.outBufferSize_bytes
         outBufferSize = bufStructs.outBufferSize * 2
 
+        
+
+        self.workgroupsize = self.determine_workgroupsize(outBufSize_bytes,
+                                                           len(last_hash),
+                                                           len(expected_hash))
+        # Allocate data
+        last_hash_array = np.frombuffer(last_hash,dtype=np.ubyte)
+        last_hash_size_uint = np.uint32(len(last_hash))     
+        result = np.zeros(outBufferSize * self.workgroupsize, dtype=self.wordType)
+        result_byte_array = np.zeros(2,dtype=np.uint32)
+        expected_hash_array = np.frombuffer(expected_hash,dtype=np.ubyte)
+
+            
+        # Allocate memory for variables on the device
+        last_hash_buffer = cl.Buffer(ctx,
+                                     cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,
+                                     hostbuf=last_hash_array)
+        result_g = cl.Buffer(ctx, cl.mem_flags.WRITE_ONLY, result.nbytes)
+        result_byte_array_buffer = cl.Buffer(ctx,cl.mem_flags.WRITE_ONLY | cl.mem_flags.COPY_HOST_PTR,
+                                            hostbuf=result_byte_array)
+        expected_hash_buffer = cl.Buffer(ctx,
+                                         cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,
+                                         hostbuf=expected_hash_array)
+
+        
+
         chunkSize = self.workgroupsize
         # Main loop is taking chunks of at most the workgroup size
         for i in range(start,end,chunkSize):
@@ -204,60 +228,56 @@ class opencl_interface:
             if i + workgroup_size>end:
                 workgroup_size = end-i
 
-            
-            last_hash_array = np.frombuffer(last_hash,dtype=np.ubyte)
-            last_hash_size_uint = np.uint32(len(last_hash))
+                result = np.zeros(outBufferSize * workgroup_size, dtype=self.wordType)
+                result_g = cl.Buffer(ctx, cl.mem_flags.WRITE_ONLY, result.nbytes)
+
+              
+            result_byte_array[0] = 0
+            result_byte_array[1] = 1
+
             start_uint = np.uint32(i)
-            result = np.zeros(outBufferSize * workgroup_size, dtype=self.wordType)
-            result_byte_array = np.zeros(workgroup_size,dtype=np.ubyte)
-            expected_hash_array = np.frombuffer(expected_hash,dtype=np.ubyte)
-
-
             
-            # Allocate memory for variables on the device
-            last_hash_buffer = cl.Buffer(ctx,
-                                         cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,
-                                         hostbuf=last_hash_array)
-            result_g = cl.Buffer(ctx, cl.mem_flags.WRITE_ONLY, result.nbytes)
-            result_byte_array_buffer = cl.Buffer(ctx,cl.mem_flags.WRITE_ONLY, result_byte_array.nbytes)
-            expected_hash_buffer = cl.Buffer(ctx,
-                                             cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,
-                                             hostbuf=expected_hash_array)
-
             # Call Kernel. Automatically takes care of block/grid distribution
             pwdim = (workgroup_size,)
            
-            func(self, pwdim, last_hash_buffer,last_hash_size_uint, result_g, result_byte_array_buffer, expected_hash_buffer,start_uint)
+            func(self,
+                 pwdim,
+                 last_hash_buffer,
+                 last_hash_size_uint, 
+                 result_g, 
+                 result_byte_array_buffer, 
+                 expected_hash_buffer,
+                 start_uint)
 
-            cl.enqueue_copy(self.queue, result_byte_array, result_byte_array_buffer)
+            cl.enqueue_copy(self.queue, 
+                            result_byte_array,
+                           result_byte_array_buffer)
+            
+            if result_byte_array[0] == 1:
+                return result_byte_array[1]
 
-            yield result_byte_array
+            #yield result_byte_array
 
         # No main return
         return None
 
-    def determine_workgroupsize(self, N_value=15):
-        devices = cl.get_platforms()[self.platform_number].get_devices()
-        wgSize = 0
-        for device in devices:
-            # Actually adjust based on invMemoryDensity!
-            N_blocks_bytes = (1 << N_value) * BLOCK_LEN_BYTES // self.inv_memory_density
-            memoryForOneCore = BLOCK_LEN_BYTES * 2 + N_blocks_bytes  # input, output & V
+    def determine_workgroupsize(self,
+                                out_buf_size,
+                                last_hash_size,
+                                expected_hash_size):
+        actual_memmory = self.available_memmory - (last_hash_size + expected_hash_size)
+        a = out_buf_size*self.wordSize
+        b = 100
+        D = b*b+4*actual_memmory*(a+1)
+        
+        workgroup_size = (-b + math.sqrt(D))/(2*(a+1))
+        workgroup_size = int(workgroup_size)*100
 
-            # ! Restrict to half the memory for now
-            coresOnDevice = (int(0.5 * device.global_mem_size) // memoryForOneCore)
-            percentUsage = 100 * memoryForOneCore * coresOnDevice / device.global_mem_size
-            percentUsage = str(percentUsage)[:4]
-            if self.debug == 1:
-                print("Using {} cores on device with global memory {}, = {}%".format(
-                    coresOnDevice, device.global_mem_size, percentUsage
-                ))
-            wgSize += device.max_work_group_size#coresOnDevice
+        return workgroup_size
 
-        if self.debug == 1:
-            print("Workgroup size determined as {}".format(wgSize))
+        
 
-        return wgSize
+        
 
 def mdPad_64_func(self, pwdLen, blockSize):
         # both parameters in bytes
@@ -297,7 +317,7 @@ class opencl_algos:
             
         to_return = np.zeros(end-start,dtype=np.ubyte)
         starting_point = 0
-        for data in self.opencl_ctx.run(bufStructs, func, None, expected_hash, mdPad_64_func, last_hash, start, end):
-            to_return[starting_point:starting_point+len(data)] = data
-            starting_point += len(data)
-        return to_return
+        return self.opencl_ctx.run(bufStructs, func, None, expected_hash, mdPad_64_func, last_hash, start, end)
+            #to_return[starting_point:starting_point+len(data)] = data
+            #starting_point += len(data)
+        #return to_return
