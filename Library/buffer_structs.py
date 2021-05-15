c9aedd52bf5830239562420d0f58de93fbed4539
YeahNotSewerSide <47860375+DoctorEenot@users.noreply.github.com>
2021-05-15 20:21:12 +0300

    Add files via upload

--- Library/buffer_structs.py
+++ Library/buffer_structs.py
@@ -9,10 +9,10 @@
 import os
 import re
 
-# Read the template in
-template = ""
-with open(os.path.join(os.path.dirname(__file__), "worker","generic","buffer_structs_template.cl"), "r") as rf:
-    template = rf.read()
+# # Read the template in
+# template = ""
+# with open(os.path.join(os.path.dirname(__file__), "worker","generic","buffer_structs_template.cl"), "r") as rf:
+    # template = rf.read()
 
 class buffer_structs:
     def __init__(self):
@@ -20,6 +20,7 @@ class buffer_structs:
         self.wordSize = 4
         
     def setMaxBufferSizes(self, max_in_bytes, max_out_bytes, max_salt_bytes=32, max_ct_bytes=0):
+        return None
         # Ensure each are a multiple of 4
         max_in_bytes += (-max_in_bytes % self.wordSize)
         max_out_bytes += (-max_out_bytes % self.wordSize)
@@ -34,10 +35,12 @@ class buffer_structs:
         self.ctBufferSize_bytes = max_ct_bytes
 
     def specifyHashSizes(self, hashBlockSize_bits, hashDigestSize_bits):
+        return None
         self.hashBlockSize_bits = hashBlockSize_bits
         self.hashDigestSize_bits = hashDigestSize_bits
 
     def setBufferSizesForHashing(self, hashMaxNumBlocks):
+        return None
         self.setMaxBufferSizes(  ((self.hashBlockSize_bits + 7) // 8) * hashMaxNumBlocks,
                             (self.hashDigestSize_bits + 7) // 8,
                             0)
@@ -46,6 +49,7 @@ class buffer_structs:
         return n + ((-n) % k)
 
     def fill_template(self):
+        return None
         rep = { "<hashBlockSize_bits>": str(self.hashBlockSize_bits),
                 "<hashDigestSize_bits>" : str(self.hashDigestSize_bits),
                 "<inBufferSize_bytes>" : str(self.inBufferSize_bytes),
@@ -60,6 +64,7 @@ class buffer_structs:
         self.code = pattern.sub(lambda m: rep[re.escape(m.group(0))], template)
 
     def specifySHA1(self, max_in_bytes=128, max_salt_bytes=32, dklen=0, max_ct_bytes=0):
+        return None
         self.specifyHashSizes(512,160)
         maxNumBlocks = 3
         self.wordSize = 4
